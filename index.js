accf57249f07e86b863f3890f2ee232d68d9b668
RiotonZong <a4214679@pccu.edu.tw>
2018-08-03 22:49:04 +0800

    0803_2249

---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = (api, opts, rootOpts) => {
+    const isProd = process.env.NODE_ENV === 'production';
+    const multiPageConfig = opts.pages;
+
+    api.chainWebpack(config => {
+        if (isProd) {
+            if (!multiPageConfig) {
+                config
+                    .plugin('html')
+                    .tap(args => {
+                        args[0].filename = api.resolve('templates/index.html');
+                        return args;
+                    });
+            } else {
+                const pages = Object.keys(multiPageConfig);
+                pages.forEach(name => {
+                    config
+                        .plugin(`html-${name}`)
+                        .tap(args => {
+                            args[0].filename = api.resolve(`templates/${name}.html`);
+                            return args;
+                        });
+                })
+            }
+        }
+    })
+}
